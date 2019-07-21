f0ce6a1e5a043291f736ef570748d75c632d6fa5
Alejandro Gonzalez <alexgve7@gmail.com>
2019-07-20 23:10:35 -0600

    Products view

--- products/classes/Product.js
+++ products/classes/Product.js
@@ -1,52 +1,68 @@
-class Product {
+class Product extends User {
+
+    constructor(){
+        super();
+    }
+    
     //Rutas de api
     getApi = 'https://playaes.000webhostapp.com/Api/products.php?request=GET&action=';
+    pictureApi = 'https://playaes.000webhostapp.com/Imports/pics/products/';
     postApi = 'https://playaes.000webhostapp.com/Api/products.php?request=POST&action=';
+    
+    route = {
+        products : '../layouts/index.html',
+        home : '../../user/layouts/home.html',
+        productView : 'viewProduct.html',
+
+        viewProduct(){
+            $(location).attr('href',this.productView);
+        }
+    }
+    
 
     get(endpoint){
         switch(endpoint){
             case 'AllList':
-                const readProducts = (rows) =>{
+                const readProducts = (rows,votes) => {               
                     let content ='';
+                    const image = this.pictureApi;
                     if(rows.length>0){
                         rows.forEach(function(row){
-                            if(row.count < 5){
-                                content += `
-                                <div class="col s12">
-                                    <div class="card">
-                                        <div class="card-content">
-                                            <p>${row.nameProduct}</p>
-                                            <p class="red-text"> No Disponible </p>
-                                        </div>
+                            content += `
+                            <div class="col s12">
+                                <div class="card horizontal z-depth-2" style="border-radius:2rem" onClick="viewProduct(${row.id})">
+                                <div class="card-image">
+                                    <img src="${ image + row.image_product }">
+                                </div>
+                                <div class="card-stacked">
+                                    <div class="card-content">
+                                        <span class="card-title">${row.nameProduct}</span>
+                                        <p> <div class="chip green accent-4 white-text">${ "$"+row.price}</div> </p>
+                                    </div>
+                                    <div class="card-action">
+                                    
                                     </div>
                                 </div>
-                            `;
-                            }else
-                            {
-                                content += `
-                                <div class="col s12">
-                                    <div class="card">
-                                        <div class="card-content">
-                                            <p>${row.nameProduct}</p>
-                                            <p class="green-text accent-4"> Disponible </p>
-                                        </div>
-                                    </div>
                                 </div>
+                            </div>
                             `;
-                            }
-                            
                         })  
                     }
                     $('#readProducts').html(content);
                 }
                 
-                $.get(this.getApi + endpoint, function(response) {
+                const userId = {
+                    id : this.Information.get().id
+                }
+
+                $.post(this.postApi + endpoint, userId ,function(response) {
                     if(isJSONString(response)){
                         const result = JSON.parse(response);
                         if(!result.status){
                             ToastError(result.exception);
                         }
-                        readProducts(result.dataset);
+                        readProducts(result.dataset, result.votes);
+                        
                     }
                     else{
                         console.log(response);
@@ -66,5 +82,5 @@ class Product {
     delete(endpoint){
 
     }
-    
+
 }
--- products/controllers/productsController.js
+++ products/controllers/productsController.js
@@ -1,9 +1,10 @@
+const person = new User();
 const products = new Product();
 $(document).ready(function () {
  spinner();
  products.get('AllList');
-
 });
+
 function spinner(){
     setTimeout(function(){ 
     $('.preloader-background').delay(1000).fadeOut('slow');
@@ -13,3 +14,10 @@ function spinner(){
         .fadeOut();
     });
 }
+
+function viewProduct(id){
+    console.log(id);
+    localStorage.setItem('idProduct', id);
+    products.route.viewProduct();
+}
+
